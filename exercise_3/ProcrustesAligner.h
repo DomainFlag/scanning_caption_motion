3b06b524f167dc8a455213002bea0a0acf2f0308
DomainFlag <Cchivriga@hotmail.com>
2019-11-09 15:49:59 +0100

    added mesh alignment using feature correspondences

---
+++ exercise_3/ProcrustesAligner.h
@@ -0,0 +1,73 @@
+#pragma once
+#include "SimpleMesh.h"
+
+class ProcrustesAligner {
+public:
+	Matrix4f estimatePose(const std::vector<Vector3f>& sourcePoints, const std::vector<Vector3f>& targetPoints) {
+		ASSERT(sourcePoints.size() == targetPoints.size() && "The number of source and target points should be the same, since every source point is matched with corresponding target point.");
+
+		// We estimate the pose between source and target points using Procrustes algorithm.
+		// Our shapes have the same scale, therefore we don't estimate scale. We estimated rotation and translation
+		// from source points to target points.
+
+		auto sourceMean = computeMean(sourcePoints);
+		auto targetMean = computeMean(targetPoints);
+		
+		Matrix3f rotation = estimateRotation(sourcePoints, sourceMean, targetPoints, targetMean);
+		Vector3f translation = computeTranslation(sourceMean, targetMean);
+
+		// To apply the pose to point x on shape X in the case of Procrustes, we execute:
+		// 1. Translation of a point to the shape Y: x' = x + t
+		// 2. Rotation of the point around the mean of shape Y: 
+		//    y = R (x' - yMean) + yMean = R (x + t - yMean) + yMean = R x + (R t - R yMean + yMean)
+		
+		// DONE: Compute the transformation matrix by using the computed rotation and translation.
+		// You can access parts of the matrix with .block(start_row, start_col, num_rows, num_cols) = elements
+		Matrix4f estimatedPose = Matrix4f::Identity();
+		estimatedPose.block(0, 0, 3, 3) = rotation;
+		estimatedPose.block(0, 3, 3, 1) = rotation * (translation - targetMean) + targetMean;
+
+		return estimatedPose;
+	}
+
+private:
+	Vector3f computeMean(const std::vector<Vector3f>& points) {
+		// DONE: Compute the mean of input points.
+		Vector3f mean = Vector3f::Zero();
+		for(const Vector3f &point: points) {
+		    mean += point;
+		}
+
+		mean /= points.size();
+
+		return mean;
+	}
+
+	Matrix3f estimateRotation(const std::vector<Vector3f>& sourcePoints, const Vector3f& sourceMean, const std::vector<Vector3f>& targetPoints, const Vector3f& targetMean) {
+		// DONE: Estimate the rotation from source to target points, following the Procrustes algorithm.
+		// To compute the singular value decomposition you can use JacobiSVD() from Eigen.
+		// Important: The covariance matrices should contain mean-centered source/target points.
+		unsigned int size = sourcePoints.size();
+        MatrixXf source(size, 3);
+        MatrixXf target(size, 3);
+
+        for(unsigned int g = 0; g < size; g++) {
+            source.row(g) = sourcePoints.at(g) - sourceMean;
+            target.row(g) = targetPoints.at(g) - targetMean;
+        }
+
+        JacobiSVD<MatrixXf> svd(target.transpose() * source, ComputeFullU | ComputeFullV);
+
+        // Compute rotation
+         Matrix3f rotation = svd.matrixU() * svd.matrixV().transpose();
+
+		return rotation;
+	}
+
+	Vector3f computeTranslation(const Vector3f& sourceMean, const Vector3f& targetMean) {
+		// DONE: Compute the translation vector from source to target points.
+		Vector3f translation = targetMean - sourceMean;
+
+		return translation;
+	}
+};
